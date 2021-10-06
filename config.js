967bbed243f168b0da90164343432682023d4e59
Vladislav <belov_vlad@list.ru>
2021-10-06 13:05:49 +0700

    MyErrorToken

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports={
   sekret:"sekret_key"
+  toketPath:'mongodb+srv://VladAdmin:jqlby@cluster0.avcz2.mongodb.net/Auth_roles?retryWrites=true&w=majority'
 }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 const AuthRouter=require('./authRouter')
 const app = express();
 const PORT = process.env.PORT || 5000;
-const urldb='mongodb+srv://VladAdmin:jqlby@cluster0.avcz2.mongodb.net/Auth_roles?retryWrites=true&w=majority'
+const urldb=require('config.js).toketPath;
 
 app.use(express.json());
 app.use('/auth',AuthRouter);
